86074a51fba9eb8c075997bb9d4b07c9306e7574
MiranBatti <miran.batti@gmail.com>
2015-10-21 15:10:59 +0200

    Deck

--- src/model/Deck.java
+++ src/model/Deck.java
@@ -2,33 +2,31 @@ package model;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
-
 
 public class Deck {
-	private ArrayList deck;
-	 
-	  public Deck ()
-	  {
-	    this.deck = new ArrayList();
-	    for (int i=0; i&lt;13; i++)
-	    {
-	      CardValue value = CardValue.values()[i];
-	 
-	      for (int j=0; j&lt;4; j++)
-	      {
-	        Card card = new Card(value, Suit.values()[j]);
-	        this.deck.add(card);
-	      }
-	    }
-	 
-	    Collections.shuffle(deck);
-	 
-	    Iterator cardIterator = deck.iterator();
-	    while (cardIterator.hasNext())
-	    {
-	      Card aCard = cardIterator.next();
-	      System.out.println(aCard.getCardValue() + " of " + aCard.getSuit());
+	private ArrayList<Card> deck;
+
+	public Deck () {
+	    this.deck = new ArrayList<Card>();
+	    for (CardValue card : CardValue.values()) {
+	    	for (Suit suit : Suit.values()) {
+				deck.add(new Card(card, suit));
+			}
 	    }
 	  }
+	
+	/**
+	 * Shuffles the deck
+	 */
+	public void shuffleDeck() {
+		Collections.shuffle(deck);
+    }
+	
+	/**
+	 * Returns deck with Cards
+	 * @return ArrayList
+	 */
+	public ArrayList<Card> getCards() {
+		return deck;
+	}
 }

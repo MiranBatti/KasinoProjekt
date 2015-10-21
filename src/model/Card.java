e769b7efa1a55c45fddd8c6de141ea5aed3d6bc9
Jonas Öster <jonas_leker@hotmail.com>
2015-10-21 14:50:57 +0200

    fixade syntax

--- src/model/Card.java
+++ src/model/Card.java
@@ -2,31 +2,26 @@ package model;
 
 public class Card {
 	private Suit suit;
-	  private CardValue cardValue;
+	private CardValue cardValue;
 	 
-	  public Card (CardValue cardValue, Suit suit)
-	  {
+	public Card (CardValue cardValue, Suit suit) {
 	    this.cardValue = cardValue;
 	    this.suit = suit;
-	  }
+	}
 	 
-	  public Suit getSuit()
-	  {
-	    return suit;
-	  }
+	public Suit getSuit() {
+		return suit;
+	}
 	 
-	  public void setSuit(Suit suit)
-	  {
+	public void setSuit(Suit suit) {
 	    this.suit = suit;
-	  }
+	}
 	 
-	  public CardValue getCardValue()
-	  {
+	public CardValue getCardValue() {
 	    return cardValue;
-	  }
+	}
 	 
-	  public void setCardValue(CardValue cardValue)
-	  {
+	public void setCardValue(CardValue cardValue) {
 	    this.cardValue = cardValue;
-	  }
+	}
 }

ecfd12db90dcecd39db0ac810f98eaff11c045b6
Jonas Öster <jonas_leker@hotmail.com>
2015-10-21 15:02:43 +0200

    player klass

---
+++ src/model/Player.java
@@ -0,0 +1,28 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Player {
+	
+	ArrayList<Card> cards;
+	
+	public Player(ArrayList<Card> cards) {
+		this.cards = cards;
+	}
+	
+	public void newCards(ArrayList<Card> cards) {
+		this.cards = cards;
+	}
+	
+	public void addCard(Card card) {
+		cards.add(card);
+	}
+	
+	public Card cardOnTable(int index) {
+		Card card = cards.get(index);
+		//skapa en checker som kollar om kortet kan gå ut
+		//
+		cards.remove(index);
+		return card;
+	}
+}

324ba7126a95b085639d7fe865dc816d533c1c07
Jonas Öster <jonas_leker@hotmail.com>
2015-10-21 15:57:58 +0200

    början deco

---
+++ src/model/Table.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Table {
+	
+	ArrayList<Card> cards;
+	
+	public Table(ArrayList<Card> cards) {
+		this.cards = cards;
+	}
+	
+	public ArrayList<Card> addCard(int cardValuePlayer) {
+		
+		ArrayList<Card> returnCards = new ArrayList<Card>();
+		
+		for (int i = 0; i < 0; i++) {
+			int cardValueDeck = 1;//cards.get(i).getCardValue();
+			
+			if (cardValuePlayer < cardValueDeck) {
+				for(int j = 0; j < 0; i++) {
+					
+				}
+			}
+			
+			
+			if (cardValueDeck == 14) {
+			}
+		}
+		
+		return null;
+	}
+}

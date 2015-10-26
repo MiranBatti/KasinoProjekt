beca69bf3da891fd30c674821f501d77d1a0d8a6
Jonas Öster <jonas_leker@hotmail.com>
2015-10-26 19:52:49 +0100

    fixat så man ser players hand
    även satt eventlistener vid klick på korten

--- src/gui/gameController.java
+++ src/gui/gameController.java
@@ -2,6 +2,7 @@ package gui;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
@@ -14,24 +15,46 @@ import model.Game;
 public class gameController implements Initializable {
 	@FXML
 	private ImageView playerCardSlot1, playerCardSlot2, playerCardSlot3, playerCardSlot4; // Måste lägga till ID i SceneBuilder i code menyn för att använda GUI objekt i koden
-	private ImageView[] playerCardSlot = new ImageView[]{playerCardSlot1, playerCardSlot2, playerCardSlot3, playerCardSlot4};
+	
+	ArrayList<ImageView> playerCardSlots;
 	
 	private Game game;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		initArrays();
 		game = new Game(App.getInstance().getPlayers());
 		playersCards();
+		setListeners();
 	}
 	
-	// Försöker få controllern att dela ut kort till spelaren när gamet startar
+	public void initArrays() {
+		playerCardSlots = new ArrayList<>();
+		playerCardSlots.add(playerCardSlot1);
+		playerCardSlots.add(playerCardSlot2);
+		playerCardSlots.add(playerCardSlot3);
+		playerCardSlots.add(playerCardSlot4);
+	}
+	
+	public void setListeners() {
+		for (ImageView imageView : playerCardSlots) {
+			imageView.setOnMouseClicked(e -> System.out.println(" ok"));
+		}
+	}
+	
+	// Delar ut kort till player, kanske ändrar koden sen
 	public void playersCards() {
-		/*ArrayList<Card> hand = game.showPlayerHand();
+		ArrayList<Card> hand = game.showPlayerHand();
 		
 		for (int i = 0; i < hand.size(); i++) {
-			String cardNameImg = hand.get(i).getSuitInt() + "_" + hand.get(i).getCardValueInt();
-			playerCardSlot[i].setImage(new Image("../resources/" + cardNameImg + ".png"));
-		}*/
+			String cardNameImg = "../resources/" + hand.get(i).getSuitInt() + "_" + hand.get(i).getCardValueInt() + ".png";
+			Image image = new Image(gameController.class.getResourceAsStream(cardNameImg));
+			playerCardSlots.get(i).setImage(image);
+		}
+	}
+	
+	public void layCardOnTable() {
+		playerCardSlot1.setOnMouseClicked(e -> System.out.println(""));
 	}
 
 }
